31f7e5c1cb1a027ca9c1b67f52e9b60cace183d8
Barath U <barathraju805@gmail.com>
2024-06-05 10:25:11 +0530

    notbook added

--- exception_python.py
+++ exception_python.py
@@ -1,9 +1,35 @@
+
+import logging
+import os
+
+#create a logger string
+logging_str="[%(asctime)s: %(levelname)s: %(module)s: %(message)s]"
+
+log_folder="logs"
+log_file = "logs/running_log.log"
+os.makedirs(log_folder, exist_ok=True)
+
+
+
+logging.basicConfig(
+    level=logging.INFO, 
+    format=logging_str,
+    
+    handlers=[
+        logging.FileHandler(log_file)
+    ]
+)
+
+logger=logging.getLogger("mylog")
+
+
+
 def division(a,b):
     try:
         out= a/b
         return out
     except Exception as e:
-        print("there are some issues with division",e)
+        logger.info(e)
         adds=a+b
         print(adds)
 
